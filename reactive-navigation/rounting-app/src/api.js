0ab0f343ab473c11d40a10991fee5992b9acfdd6
Manjosh R <manjosh.1990@yahoo.com>
2023-08-07 20:28:47 +0530

    added fake authentication on the login page

--- reactive-navigation/rounting-app/src/api.js
+++ reactive-navigation/rounting-app/src/api.js
@@ -13,8 +13,8 @@ export async function getVans(id) {
 }
 
 export async function getHostVans(id) {
-  const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-  const res = await fetch(url)
+  const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
+  const res = await fetch(url);
   if (!res.ok) {
     throw Object.assign(new Error("Failed to fetch vans"), {
       message: "Failed to fetch vans",
@@ -22,6 +22,24 @@ export async function getHostVans(id) {
       status: res.status,
     });
   }
-  const data = await res.json()
-  return data.vans
+  const data = await res.json();
+  return data.vans;
+}
+
+export async function loginUser(creds) {
+  const res = await fetch("/api/login", {
+    method: "post",
+    body: JSON.stringify(creds),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw Object.assign(new Error(), {
+      message: data.message,
+      statusText: res.statusText,
+      status: res.status,
+    });
+  }
+
+  return data;
 }
--- reactive-navigation/rounting-app/src/pages/Login.jsx
+++ reactive-navigation/rounting-app/src/pages/Login.jsx
@@ -1,45 +1,59 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { loginUser } from "../api";
 
-export function loader({request}){
+export function loader({ request }) {
   const url = new URL(request.url);
   return url.searchParams.get("message");
 }
 
-export default function Login(){
-  const[loginFormData,setLoginFormData] = useState({email:"",password:""});
-
+export default function Login() {
+  const [loginFormData, setLoginFormData] = useState({ email: "", password: "" });
+  const [status, setStatus] = useState("idle");
+  const [error,setError] = useState(null);
   const message = useLoaderData();
-  function handleChange(e){
-    const {name ,value} = e.target;
-    setLoginFormData(prev=> ({
-      ...prev,[name]:value
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setLoginFormData(prev => ({
+      ...prev, [name]: value
     }))
   }
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault()
+    setStatus("submitting");
+    setError(null);
     console.log(loginFormData);
+    loginUser(loginFormData)
+      .then(
+        data => console.log(data)
+      ).catch(error => {
+        console.log(error.message)
+        setError(error)
+      }).finally(() => {
+        setStatus("idle")
+      })
   }
-  return(<div className="login-container">
+  return (<div className="login-container">
     {message && <h3 className="red">{message}</h3>}
+    {error && <h3 className="red">{error.message}</h3>}
     <h1>Sign in to your account</h1>
     <form onSubmit={handleSubmit} className="login-form">
-        <input
-          name="email"
-          type="email"
-          onChange={handleChange}
-          placeholder="Email address"
-          value={loginFormData.email}
-          />
-          <input
-          name="password"
-          type="password"
-          onChange={handleChange}
-          placeholder="Password"
-          value={loginFormData.password}
-          />
-          <button>Log in</button>
+      <input
+        name="email"
+        type="email"
+        onChange={handleChange}
+        placeholder="Email address"
+        value={loginFormData.email}
+      />
+      <input
+        name="password"
+        type="password"
+        onChange={handleChange}
+        placeholder="Password"
+        value={loginFormData.password}
+      />
+      <button disabled={status === "submitting"}>{status==="submitting" ?"Logging in...": "Log in"}</button>
     </form>
   </div>)
 }
