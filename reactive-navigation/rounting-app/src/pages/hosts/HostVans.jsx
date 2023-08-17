bc78e73cbf0116e4ea7cd3131a45a2197036198e
Manjosh R <manjosh.1990@yahoo.com>
2023-08-17 21:09:07 +0530

    implemented defer,Await and Suspense

--- reactive-navigation/rounting-app/src/pages/hosts/HostVans.jsx
+++ reactive-navigation/rounting-app/src/pages/hosts/HostVans.jsx
@@ -1,35 +1,43 @@
-import React from "react";
-import { Link ,useLoaderData} from "react-router-dom";
+import React, { Suspense } from "react";
+import { Link, useLoaderData, defer, Await } from "react-router-dom";
 import { getHostVans } from "../../api";
 import { requireAuth } from "../../utils";
-export async function loader({request}){
+export async function loader({ request }) {
   await requireAuth(request)
-  return getHostVans();
+  return defer({ vans: getHostVans() });
 }
 export default function HostVans() {
-
-  const vans = useLoaderData();
-  const vanList = vans.map(item => {
-    return (
-      <Link
-        to={item.id}
-        key={item.id}
-        className="host-van-link-wrapper"
-      >
-        <div key={item.id} className="van-single">
-          <img src={item.imageUrl} alt="vanImage" />
-          <div className="van-info">
-            <h3>{item.name}</h3>
-            <p>${item.price}/day</p>
+  const vansDataPromise = useLoaderData();
+  function renderVanElements(vans) {
+    const vanList = vans.map(item => {
+      return (
+        <Link
+          to={item.id}
+          key={item.id}
+          className="host-van-link-wrapper"
+        >
+          <div key={item.id} className="van-single">
+            <img src={item.imageUrl} alt="vanImage" />
+            <div className="van-info">
+              <h3>{item.name}</h3>
+              <p>${item.price}/day</p>
+            </div>
           </div>
-        </div>
-      </Link>
-    );
-  })
+        </Link>
+      );
+    })
+    return (<>
+      <div className="host-vans-list">
+        <section>{vanList}</section>
+      </div>
+    </>)
+  }
   return (<section>
     <h1 className="host-vans-title">Your listed vans</h1>
-    <div className="host-vans-list">
-      <section>{vanList}</section>
-    </div>
+    <Suspense fallback={<h2>loading ...</h2>}>
+      <Await resolve={vansDataPromise.vans}>
+        {renderVanElements}
+      </Await>
+    </Suspense>
   </section>)
 }
--- reactive-navigation/rounting-app/src/pages/vans/Vans.jsx
+++ reactive-navigation/rounting-app/src/pages/vans/Vans.jsx
@@ -1,37 +1,18 @@
-import React from "react";
-import { Link, useSearchParams, useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Link, useSearchParams, useLoaderData, defer, Await } from "react-router-dom";
 import { getVans } from "../../api";
 
 export function loader() {
-  return getVans();
+  return defer({ vans: getVans() })
 }
 
 export default function Vans() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const vans = useLoaderData();
-  console.log(vans);
+  const dataPromise = useLoaderData();
 
   const typeFilter = searchParams.get("type");
-  const displayedVans = typeFilter
-    ? vans.filter(van => van.type === typeFilter)
-    : vans
-  console.log("typeFilter :" + typeFilter);
-  const vanElements = displayedVans.map(van => {
-    return (
-      <div key={van.id} className="van-tile">
-        <Link to={van.id} state={{ search: `?${searchParams.toString()}`, type: typeFilter }}>
-          <img src={van.imageUrl} alt={van.name} />
-          <div className="van-info">
-            <h3>{van.name}</h3>
-            <p>${van.price}<span>/day</span></p>
-          </div>
-          <i className={`van-type ${van.type} selected`}>{van.type}</i>
-        </Link>
-      </div>
-    )
-  })
-  function handleFilterChange(key, value) {
 
+  function handleFilterChange(key, value) {
     setSearchParams(prevParams => {
       if (value === null) {
         prevParams.delete(key)
@@ -41,10 +22,26 @@ export default function Vans() {
       return prevParams
     })
   }
-
-  return (
-    <div className="van-list-container">
-      <h1>Explore our van options</h1>
+  function renderVanElements(vans) {
+    const displayedVans = typeFilter
+      ? vans.filter(van => van.type === typeFilter)
+      : vans
+    console.log("typeFilter :" + typeFilter);
+    const vanElements = displayedVans.map(van => {
+      return (
+        <div key={van.id} className="van-tile">
+          <Link to={van.id} state={{ search: `?${searchParams.toString()}`, type: typeFilter }}>
+            <img src={van.imageUrl} alt={van.name} />
+            <div className="van-info">
+              <h3>{van.name}</h3>
+              <p>${van.price}<span>/day</span></p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          </Link>
+        </div>
+      )
+    })
+    return (<>
       <div className="van-list-filter-buttons">
         <button
           onClick={() => handleFilterChange("type", "simple")}
@@ -69,6 +66,16 @@ export default function Vans() {
       <div className="van-list">
         {vanElements}
       </div>
+    </>)
+  }
+  return (
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Await resolve={dataPromise.vans}>
+          {renderVanElements}
+        </Await>
+      </Suspense>
     </div>
   )
 }
